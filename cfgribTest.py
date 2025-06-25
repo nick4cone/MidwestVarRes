a682543288cbaacf7de8d12bda64f432ec3155e0
Nicholas Forcone <nforcone@icloud.com>
2025-06-25 13:09:15 -0600

    path to HRRR on native levels

--- cfgribTest.py
+++ cfgribTest.py
@@ -3,9 +3,7 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-# path = "/glade/derecho/scratch/nforcone/HRRR_data/hrrr/20200810/hrrr.t00z.wrfsfcf00.grib2"
-# path = "/glade/derecho/scratch/nforcone/HRRR_testing/hrrr/20200810/hrrr.t00z.wrfsfcf00.grib2"
-path = "/glade/derecho/scratch/nforcone/HRRR_google/hrrr/20200810/hrrr.t00z.wrfsfcf00.grib2"
+path = "/glade/derecho/scratch/nforcone/HRRR_native/hrrr.20200810_conus_hrrr.t00z.wrfnatf00.grib2"
 
 ds_sfc_instant = xr.open_dataset(path, engine='cfgrib', backend_kwargs={'filter_by_keys': {'stepType': 'instant', 'typeOfLevel': 'surface'}})
 print(ds_sfc_instant.data_vars)
